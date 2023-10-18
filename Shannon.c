7ec62b7cc08f1786813dea9f7aa7c26a03b22835
Shreyas <98160764+Justabeginner4@users.noreply.github.com>
2023-10-18 18:10:37 +0530

    Update Shannon.c

--- Shannon.c
+++ Shannon.c
@@ -1,134 +1,433 @@
-#include <stdio.h>
-#include <string.h>
+#include<iostream>
+#include<bits/stdc++.h>
+#include<vector>
+#include<stdlib.h>
+#include<math.h>
+#include<fstream>
+#include<windows.h>
+using namespace std;
 
-struct node {
-    char sym[20];
-    float pro;
-    int arr[20];
-    int top;
+int actual;
+class OP{
+	public:
+	int freq;
+	char ch;
+	string codw;
+	void getd()
+	{
+		cout<<"Enter Character:";cin>>ch;
+		cout<<"Enter Frequency:";cin>>freq;
+		this->codw="";
+	}
+	void getd(char dh,int preq)
+	{
+		this->ch=dh;
+		this->freq=preq;
+		this->codw="";
+	}
+	
+	void display()
+	{
+		cout<<"Character:"<<ch<<"     Frequency:"<<freq<<endl;
+	}
+	void cop()
+	{
+		cout<<"Code of "<<ch<<"="<<codw<<endl;
+	}
 };
+OP pi[127];
 
-typedef struct node node;
+class chunks{
+	public:
+		string b8;
+		char d8;
+		int i8;
+};
+chunks cnks[500];
+void shan(int s,int e)
+{
+	if(s<e)
+	{
+		int sum_1=0,sum_2=0;
+		int mind=10000;
+		int t=0;
+		int diff=0;
+		int pt;
+		int i,j,k,cmp;
+		
+		for(cmp=0;cmp<(e-s);cmp++)	
+		{
 
-void shannon(int l, int h, node p[]);
-void sortByProbability(int n, node p[]);
-void display(int n, node p[]);
-
-int main() {
-    int n, i, j;
-    float total = 0;
-    char ch[20];
-    node p[20];
-
-    printf("Enter number of symbols\t: ");
-    n = 5;
-    printf("%d\n", n);
-
-    for (i = 0; i < n; i++) {
-        printf("Enter symbol %d : ", i + 1);
-        ch[0] = 65 + i;
-        ch[1] = '\0';
-        printf("%s\n", ch);
-
-        strcpy(p[i].sym, ch);
-    }
-
-    float x[] = {0.22, 0.28, 0.15, 0.30, 0.05};
-    for (i = 0; i < n; i++) {
-        printf("\nEnter probability of %s : ", p[i].sym);
-        printf("%f\n", x[i]);
-
-        p[i].pro = x[i];
-        total = total + p[i].pro;
-
-        if (total > 1) {
-            printf("Invalid. Enter new values\n");
-            total = total - p[i].pro;
-            i--;
-        }
-    }
-
-    p[i].pro = 1 - total;
-
-    sortByProbability(n, p);
-
-    for (i = 0; i < n; i++)
-        p[i].top = -1;
-
-    shannon(0, n - 1, p);
-
-    display(n, p);
-    return 0;
+			for(k=s;k<=s+cmp;k++)
+			{
+				sum_1=sum_1+pi[k].freq;
+			}
+			for(j=s+cmp+1;j<=e;j++)
+			{
+				sum_2=sum_2+pi[j].freq;
+			}
+			diff=abs(sum_2-sum_1);	
+			if(diff<=mind)
+			{
+				mind=diff;
+				pt=cmp+s;
+			}
+			sum_1=0;
+			sum_2=0;
+		}
+			for(i=s;i<=pt;i++)
+			{
+				pi[i].codw=pi[i].codw+"1";
+			}
+			cout<<endl;
+			for(i=pt+1;i<=e;i++)
+			{
+				pi[i].codw=pi[i].codw+"0";
+			}
+			int tmpp=pt;
+			shan(tmpp+1,e);
+			shan(s,tmpp);
+	}
 }
 
-void shannon(int l, int h, node p[]) {
-    float pack1 = 0, pack2 = 0, diff1 = 0, diff2 = 0;
-    int i, d, k, j;
-    if ((l + 1) == h || l == h || l > h) {
-        if (l == h || l > h)
-            return;
-        p[h].arr[++(p[h].top)] = 0;
-        p[l].arr[++(p[l].top)] = 1;
-        return;
-    } else {
-        for (i = l; i <= h - 1; i++)
-            pack1 = pack1 + p[i].pro;
-        pack2 = pack2 + p[h].pro;
-        diff1 = pack1 - pack2;
-        if (diff1 < 0)
-            diff1 = diff1 * -1;
-        j = 2;
-        while (j != h - l + 1) {
-            k = h - j;
-            pack1 = pack2 = 0;
-            for (i = l; i <= k; i++)
-                pack1 = pack1 + p[i].pro;
-            for (i = h; i > k; i--)
-                pack2 = pack2 + p[i].pro;
-            diff2 = pack1 - pack2;
-            if (diff2 < 0)
-                diff2 = diff2 * -1;
-            if (diff2 >= diff1)
-                break;
-            diff1 = diff2;
-            j++;
-        }
-        k++;
-        for (i = l; i <= k; i++)
-            p[i].arr[++(p[i].top)] = 1;
-        for (i = k + 1; i <= h; i++)
-            p[i].arr[++(p[i].top)] = 0;
-
-        shannon(l, k, p);
-        shannon(k + 1, h, p);
-    }
+void rdfile()
+{
+	ifstream inFile;
+   	char ch; 
+	cout<<"Reading file = ab.txt";
+	Sleep(500);
+	cout<<".";
+	Sleep(500);
+	cout<<".";
+	Sleep(500);
+	cout<<".";
+	Sleep(500);
+	cout<<".";
+	Sleep(500);
+	cout<<".";
+	Sleep(500);
+	cout<<".";
+	inFile.open("ab.txt");
+	if (!inFile)
+	{
+	  cout << "The input file could not be opened."<<endl;
+	  exit(0);
+	}
+	for(int i=0;i<127;i++)
+	{
+		cout<<"Char("<<i<<")="<<char(i)<<endl;
+		pi[i].getd(char(i),0);
+	}
+   	ch = inFile.get();
+   	while (ch != EOF)
+	{
+		pi[int(ch)].freq++;
+    	ch = inFile.get();
+   	}
 }
 
-void sortByProbability(int n, node p[]) {
-    int i, j;
-    node temp;
-    for (j = 1; j <= n - 1; j++) {
-        for (i = 0; i < n - 1; i++) {
-            if ((p[i].pro) > (p[i + 1].pro)) {
-                temp.pro = p[i].pro;
-                strcpy(temp.sym, p[i].sym);
+void wtfile(int h)
+{
+	ifstream inFile;   
+	ofstream outfile;	
+   	outfile.open("1_Codeword.txt");	
+   	
+	char ch; 
+	cout<<endl;
+	cout<<"Reading file = ab.txt"<<endl;
+	cout<<"Generating 1_Codeword.txt file";
+	Sleep(500);
+	cout<<".";
+	Sleep(500);
+	cout<<".";
+	Sleep(500);
+	cout<<".";
+	Sleep(500);
+	cout<<".";
+	Sleep(500);
+	cout<<".";
+	Sleep(500);
+	cout<<".";
+	inFile.open("ab.txt");
+	if (!inFile)
+	{
+	  cout << "The input file could not be opened."<<endl;
+	  exit(0);
+	}
 
-                p[i].pro = p[i + 1].pro;
-                strcpy(p[i].sym, p[i + 1].sym);
-
-                p[i + 1].pro = temp.pro;
-                strcpy(p[i + 1].sym, temp.sym);
-            }
-        }
-    }
+   	ch = inFile.get();
+   	int i;
+	while (ch != EOF)
+	{
+		i=h;
+		while(pi[i].ch!=ch)
+		{
+			i++;
+		}
+		outfile<<pi[i].codw;
+    	ch = inFile.get();
+   	}
+   	outfile.close();
+   	cout<<"File Generated!"<<endl;
 }
 
-void display(int n, node p[]) {
-    int i, j;
-    printf("\n\n\n\tSymbol\tProbability\tCode");
-    for (i = n - 1; i >= 0; i--) {
-        printf("\n\t%s\t\t%f\t", p[i].sym, p[i].pro);
-        for (j = 0; j <= p[i].top; j++)
-            printf("%d", p[i].arr[j]);
-    }
+int binTOdec(string binaryString)
+{  
+	int value = 0;
+	int indexCounter = 0;
+	for(int i=binaryString.length()-1;i>=0;i--)
+	{
+	    if(binaryString[i]=='1'){
+	        value += pow(2, indexCounter);
+	    }
+    indexCounter++;
+	}
+	return value;
+}
+
+
+void dcp()
+{
+	ifstream inFile; 
+	ofstream outfile;
+	outfile.open("2_compressed.txt");	
+	char ch; 
+	cout<<"Reading 1_Codeword.txt file"<<endl;
+	cout<<"Generating 2_compressed.txt file.";
+	Sleep(500);
+	cout<<".";
+	Sleep(500);
+	cout<<".";
+	Sleep(500);
+	cout<<".";
+	Sleep(500);
+	cout<<".";
+	Sleep(500);
+	cout<<".";
+	Sleep(500);
+	cout<<".";
+	inFile.open("1_codeword.txt");
+	if (!inFile)
+	{
+	  cout << "The input file could not be opened."<<endl;
+	  exit(0);
+	}
+   	ch = inFile.get();
+   	int i=0,lmt=0;
+	cout<<"\tCHAR\tINT\tBINARY\tCint"<<endl;
+	while (ch != EOF)
+	{
+		cnks[i].b8=cnks[i].b8+ch;
+		while(cnks[i].b8.length()!=8)
+		{
+			ch = inFile.get();
+			if(ch == EOF)
+			{
+				lmt++;
+				ch = '0';
+			}
+			cnks[i].b8=cnks[i].b8+ch;	
+		}
+//		cnks[i].i8 = binTOdec(cnks[i].b8);
+//		cnks[i].d8 = char(cnks[i].i8);
+		cnks[i].d8 = char(binTOdec(cnks[i].b8));
+		cnks[i].i8 = int(cnks[i].d8);
+		cout<<"\t"<<cnks[i].d8<<"\t"<<cnks[i].i8<<"\t"<<cnks[i].b8<<"\t"<<int(cnks[i].d8)<<endl;
+		outfile<<cnks[i].d8;
+		ch = inFile.get();
+    	i++;
+   	}
+   	actual = (i*8)-lmt;
+   	outfile.close();
+   	cout<<"\nFile 2_compressed.txt Generated!"<<endl;
+   	Sleep(100);
+}
+
+
+
+void fina()
+{
+	ifstream inFile;   // input file
+	ofstream outfile;	//output file
+   	
+	outfile.open("3_BinaryOfCompressed.txt");	
+   	
+	char ch; 
+	cout<<"Reading file = 2_Compressed.txt"<<endl;
+	cout<<"Generating 3_BinaryOfCompressed.txt file.";
+	Sleep(500);
+	cout<<".";
+	Sleep(500);
+	cout<<".";
+	Sleep(500);
+	cout<<".";
+	Sleep(500);
+	cout<<".";
+	Sleep(500);
+	cout<<".";
+	Sleep(500);
+	cout<<".";
+	inFile.open("2_compressed.txt");
+	if (!inFile)
+	{
+	  cout << "The input file could not be opened."<<endl;
+	  exit(0);
+	}
+   	ch = inFile.get();
+   	int i=0;
+   	cout<<"Results In 3_BinaryOfCompressed"<<endl;
+   	cout<<"Character\tBinary"<<endl;
+	
+	while(ch != EOF)
+	{
+		while (ch != EOF)
+		{
+	      	cout <<"\t"<< ch<<"\t"<<int(ch)<<"\t";
+			bitset<8> bin_x(ch);
+			outfile<<bin_x;
+			cout<<"binary ="<<bin_x<<endl;
+			ch = inFile.get();
+			cout<<"CHAR"<<ch<<"\t"<<int(ch)<<"\t\t";
+		}
+		
+	   	ch = inFile.get();
+	}
+
+   	
+	cout<<"\nGenerated 3_BinaryOfCompressed.txt file!"<<endl;
+   	Sleep(1000);
+   	outfile.close();
+	
+}
+
+void las(int h)
+{
+	ifstream inFile;
+	ofstream outfile;
+	
+	inFile.open("3_BinaryOfCompressed.txt");
+	outfile.open("4_Finalizedfile.txt");
+	cout<<"Reading file = 3_BinaryOfCompressed.txt"<<endl;
+	cout<<"Generating 4_Finalizedfile.txt file.";
+	Sleep(500);
+	cout<<".";
+	Sleep(500);
+	cout<<".";
+	Sleep(500);
+	cout<<".";
+	Sleep(500);
+	cout<<".";
+	Sleep(500);
+	cout<<".";
+	Sleep(500);
+	cout<<".";
+	if(!inFile)
+	{
+		cout << "The input file could not be opened."<<endl;
+	  	exit(0);
+	}
+	char ch;
+	ch = inFile.get();
+	string str="";
+	for(int i=0;i<actual;i++)
+	{
+		str=str+ch;
+		for(int k=h;k<127;k++)
+		{
+			if(pi[k].codw==str)
+			{
+				outfile<<pi[k].ch;
+				str="";
+			}
+		}
+		ch = inFile.get();
+	}
+	outfile.close();
+	cout<<"\nGenerated 4_Finalizedfile.txt file!"<<endl;
+	
+}
+
+void deta()
+{
+	ifstream iff;
+	
+	iff.open("ab.txt"); //enter Your input file path with name
+	iff >> std::noskipws;
+	char ch;
+	int siz=0;
+	while(iff>>ch)
+	{
+		siz++;
+	}
+	cout<<"\n\tFile Name\t|\tSize";
+	cout<<"\n\t ab.txt||\t"<<siz<<"bytes";
+	iff.close();
+	iff >> std::noskipws;	
+	iff.open("2_compressed.txt"); //enter Your input file path with name
+	siz=0;
+	while(iff>>ch)
+	{
+		siz++;
+	}
+	cout<<"\n\t2_compressed.txt||\t"<<siz<<"bytes";
+	iff.close();
+}
+
+int main()
+{
+	int i,j,k,n;
+	int sum_1=0,sum_2=0;
+	int mind=10000;
+	int t=0;
+	int diff=0;
+	int pt;
+	n=127;
+	rdfile();
+
+	for(i=0;i<n-1;i++)
+	{
+		for(j=0;j<n-i-1;j++)
+		{
+			if(pi[j].freq>pi[j+1].freq)
+			{
+				OP tmp;
+				tmp=pi[j];
+				pi[j]=pi[j+1];
+				pi[j+1]=tmp;
+			}
+		}
+	}
+	int h;
+	for(i=0;i<n;i++)
+	{
+		if(pi[i].freq!=0)
+		{
+			h=i;	
+			break;
+		}
+	}
+
+	for(i=h;i<n;i++)
+	{
+		cout<<i;
+		pi[i].display();
+	}	
+
+	shan(h,n-1);
+	ofstream codfile;//codeword file
+	codfile.open("codfile.txt");
+	
+	for(i=h;i<n;i++)
+	{
+		codfile<<pi[i].ch<<" ||  Freq="<<pi[i].freq<<"  ||   Codeword="<<pi[i].codw<<endl;
+		pi[i].cop();
+	}
+	codfile.close();
+	wtfile(h);
+	dcp();
+	cout<<"ACTUAL==================================================="<<actual<<endl;
+	fina();
+	las(h);
+	deta();
+	return 0;
 }
